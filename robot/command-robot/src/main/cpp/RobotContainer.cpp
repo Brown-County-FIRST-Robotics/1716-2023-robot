a7daa878f395cbfe7c411b1418639ace9c13a8f6
Colin <cfinn68916@gmail.com>
2022-12-08 18:04:52 -0600

    add more working code to command based

--- robot/command-robot/src/main/cpp/RobotContainer.cpp
+++ robot/command-robot/src/main/cpp/RobotContainer.cpp
@@ -1,17 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 #include "RobotContainer.h"
 
-#include <frc2/command/button/JoystickButton.h>
-#include <frc/XboxController.h> //used for enumerators
+RobotContainer::RobotContainer() : m_autonomousCommand(&m_subsystem) {
+  // Initialize all of your commands and subsystems here
 
-RobotContainer::RobotContainer() {
+  // Configure the button bindings
   ConfigureButtonBindings();
 }
 
 void RobotContainer::ConfigureButtonBindings() {
-  // Configure your button bindings to commands here
-	(frc2::JoystickButton(p_controller, frc::XboxController::Button::kA)
-		&& frc2::JoystickButton(p_controller, frc::XboxController::Button::kB))
-		.WhileActiveOnce(SpinAll(p_motors)); //if both are held, spin both (have to use WhileActiveOnce() because of && (docs))
-	frc2::JoystickButton(p_controller, frc::XboxController::Button::kA).WhenHeld(SpinTfx(p_motors));
-	frc2::JoystickButton(p_controller, frc::XboxController::Button::kB).WhenHeld(SpinTsrx(p_motors));
+  // Configure your button bindings here
+}
+
+frc2::Command* RobotContainer::GetAutonomousCommand() {
+  // An example command will be run in autonomous
+  return &m_autonomousCommand;
 }
