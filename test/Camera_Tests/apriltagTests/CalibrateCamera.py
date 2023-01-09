f285a7710427848ec45bb74a7c8841a2f9cb253a
Colin <cfinn68916@gmail.com>
2023-01-09 17:20:41 -0600

    add apriltag files

---
+++ test/Camera_Tests/apriltagTests/CalibrateCamera.py
@@ -0,0 +1,151 @@
+#mostly coppied from https://github.com/kyle-bersani/opencv-examples/blob/master/CalibrationByCharucoBoard/CalibrateCamera.py
+#with a little from https://mecaruco2.readthedocs.io/en/latest/notebooks_rst/Aruco/sandbox/ludovic/aruco_calibration_rotation.html
+
+
+
+# System information:
+# - Linux Mint 18.1 Cinnamon 64-bit
+# - Python 2.7 with OpenCV 3.2.0
+
+import numpy
+import cv2
+from cv2 import aruco
+import pickle
+import glob, json
+
+
+# ChAruco board variables
+CHARUCOBOARD_ROWCOUNT = 7
+CHARUCOBOARD_COLCOUNT = 5 
+ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_5X5_1000)
+
+# Create constants to be passed into OpenCV and Aruco methods
+CHARUCO_BOARD = aruco.CharucoBoard_create(
+        squaresX=CHARUCOBOARD_COLCOUNT,
+        squaresY=CHARUCOBOARD_ROWCOUNT,
+        squareLength=0.04,
+        markerLength=0.02,
+        dictionary=ARUCO_DICT)
+
+
+import sys
+if 'generate' in sys.argv[1:]:
+    imboard=CHARUCO_BOARD.draw((2000, 2000))
+    cv2.imwrite("chessboard.tiff", imboard)
+    print('chessboard.tiff written')
+    sys.exit()
+
+# Create the arrays and variables we'll use to store info like corners and IDs from images processed
+corners_all = [] # Corners discovered in all images processed
+ids_all = [] # Aruco ids corresponding to corners discovered
+image_size = None # Determined at runtime
+
+
+# This requires a set of images or a video taken with the camera you want to calibrate
+# I'm using a set of images taken with the camera with the naming convention:
+# 'camera-pic-of-charucoboard-<NUMBER>.jpg'
+# All images used should be the same size, which if taken with the same camera shouldn't be a problem
+
+
+#images = glob.glob('/tmp/2022*.jpg')
+images = [cv2.imread(i) for i in sys.argv[1:]]
+image_count = 0
+
+# Loop through images glob'ed
+for img in images:
+    # Open the image
+    # Grayscale the image
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Find aruco markers in the query image
+    corners, ids, _ = aruco.detectMarkers(
+            image=gray,
+            dictionary=ARUCO_DICT)
+
+#    cv2.imshow('Charuco board', img)
+    if not corners: #didn't see board at all
+        print('no corners')
+        cv2.imshow('Charuco board', img)
+        continue
+    print('found')
+    # Outline the aruco markers found in our query image
+    img = aruco.drawDetectedMarkers(
+            image=img, 
+            corners=corners)
+
+    # Get charuco corners and ids from detected aruco markers
+    response, charuco_corners, charuco_ids = aruco.interpolateCornersCharuco(
+            markerCorners=corners,
+            markerIds=ids,
+            image=gray,
+            board=CHARUCO_BOARD)
+
+    # If a Charuco board was found, let's collect image/corner points
+    # Requiring at least 20 squares
+    if response > 20:
+        print('runnning calibration math')
+        # Add these corners and ids to our calibration arrays
+        corners_all.append(charuco_corners)
+        ids_all.append(charuco_ids)
+        
+        # Draw the Charuco board we've detected to show our calibrator the board was properly detected
+        img = aruco.drawDetectedCornersCharuco(
+                image=img,
+                charucoCorners=charuco_corners,
+                charucoIds=charuco_ids)
+       
+        # If our image size is unknown, set it now
+        if not image_size:
+            image_size = gray.shape[::-1]
+    
+        # Reproportion the image, maxing width or height at 1000
+        proportion = max(img.shape) / 1000.0
+        img = cv2.resize(img, (int(img.shape[1]/proportion), int(img.shape[0]/proportion)))
+        # Pause to display each image, waiting for key press
+        #cv2.imshow('Charuco board', img)
+        #cv2.waitKey(0)
+        image_count+=1
+    else:
+        print(f"Not able to detect a charuco board in image")
+
+# Destroy any open CV windows
+cv2.destroyAllWindows()
+
+# Make sure at least one image was found
+if len(images) < 1:
+    # Calibration failed because there were no images, warn the user
+    print("Calibration was unsuccessful. No images of charucoboards were found. Add images of charucoboards and use or alter the naming conventions used in this file.")
+    # Exit for failure
+    exit()
+
+# Make sure we were able to calibrate on at least one charucoboard by checking
+# if we ever determined the image size
+if not image_size:
+    # Calibration failed because we didn't see any charucoboards of the PatternSize used
+    print("Calibration was unsuccessful. We couldn't detect charucoboards in any of the images supplied. Try changing the patternSize passed into Charucoboard_create(), or try different pictures of charucoboards.")
+    # Exit for failure
+    exit()
+
+# Now that we've seen all of our images, perform the camera calibration
+# based on the set of points we've discovered
+calibration, cameraMatrix, distCoeffs, rvecs, tvecs = aruco.calibrateCameraCharuco(
+        charucoCorners=corners_all,
+        charucoIds=ids_all,
+        board=CHARUCO_BOARD,
+        imageSize=image_size,
+        cameraMatrix=None,
+        distCoeffs=None)
+    
+# Print matrix and distortion coefficient to the console
+print(cameraMatrix)
+print(distCoeffs)
+    
+# Save values to be used where matrix+dist is required, for instance for posture estimation
+# I save files in a pickle file, but you can use yaml or whatever works for you
+f = open('calibration.pckl', 'w')
+json.dump(([list(i) for i in cameraMatrix], [list(i) for i in distCoeffs]), f)
+f.close()
+    
+# Print to console our success
+print(f'Calibration successful. Calibration file used: calibration.pckl')
+
---
+++ test/Camera_Tests/apriltagTests/main.py
@@ -0,0 +1,160 @@
+# (x,y,z, roll, pitch, yaw)
+'''
+------ <default position
+y/roll
+|
+|
+. ____x/pitch
+z/yaw
+'''
+import time
+
+import apriltag
+import cv2
+import numpy as np
+import sys, os
+import random
+import json
+
+# I am getting the tag sizes from a json file for all apriltags
+
+# tag_sizes, tvecs, rvecs = get_extrinsics(json_file)
+
+cam = cv2.VideoCapture()
+cam.open('/dev/video3')
+
+tag_size = 8/2.54
+
+video_size = (cam.get(cv2.CAP_PROP_FRAME_WIDTH), cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+print(video_size)
+
+#focal_length = video_size[1]
+#camera_center = (video_size[1] / 2, video_size[0] / 2)
+
+f=open('logi.json','r')
+mtx,dist=json.loads(f.read())
+print(mtx)
+mtx=np.array(mtx)
+dist=np.array(dist)
+
+def convert(pt):
+    return (int(pt[0]), int(pt[1]))
+
+
+apriltags_found=0
+data=[]
+while apriltags_found<500:
+    img = cam.read()[1]
+
+    # Convert Images to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # AprilTag detector options
+    options = apriltag.DetectorOptions(families='tag16h5',
+                                    border=1,
+                                    nthreads=4,
+                                    quad_decimate=1.0,
+                                    quad_blur=0.0,
+                                    refine_edges=True,
+                                    refine_decode=False,
+                                    refine_pose=True,
+                                    debug=False,
+                                    quad_contours=True)
+
+    detector = apriltag.Detector(options)
+
+    # Detect the apriltags in the image
+    detection_results = detector.detect(gray)
+
+    # Amount of april tags detected
+    num_detections = len(detection_results)
+
+    imgPointsArr = []
+    objPointsArr = []
+    opointsArr = []
+
+    if num_detections > 0:
+        for i, detection in enumerate(detection_results):
+            if detection.tag_id!=3:
+                continue
+            (ptA, ptB, ptC, ptD) = detection.corners
+            ptA = convert(ptA)
+            ptB = convert(ptB)
+            ptC = convert(ptC)
+            ptD = convert(ptD)
+
+            #cv2.line(img, ptA, ptB, (0, 0, 255), 2)
+            #/cv2.line(img, ptB, ptC, (0, 0, 255), 2)
+            #cv2.line(img, ptC, ptD, (0, 0, 255), 2)
+            #cv2.line(img, ptD, ptA, (0, 0, 255), 2)
+
+            if mtx is not None:
+                imagePoints = detection.corners.reshape(1,4,2)
+
+                # Get the tag size from .json file
+
+                ob_pt1 = [-tag_size/2, -tag_size/2, 0.0]
+                ob_pt2 = [ tag_size/2, -tag_size/2, 0.0]
+                ob_pt3 = [ tag_size/2,  tag_size/2, 0.0]
+                ob_pt4 = [-tag_size/2,  tag_size/2, 0.0]
+                ob_pts = ob_pt1 + ob_pt2 + ob_pt3 + ob_pt4
+                object_pts = np.array(ob_pts).reshape(4,3)
+
+                opoints = np.array([
+                    -1, -1, 0,
+                    1, -1, 0,
+                    1,  1, 0,
+                    -1,  1, 0,
+                    -1, -1, -2*1,
+                    1, -1, -2*1,
+                    1,  1, -2*1,
+                    -1,  1, -2*1,
+                ]).reshape(-1, 1, 3) * 0.5*tag_size
+
+
+                imgPointsArr.append(imagePoints)
+                objPointsArr.append(object_pts)
+                opointsArr.append(opoints)
+
+                # mtx - the camera calibration's intrinsics
+                good, prvecs, ptvecs = cv2.solvePnP(object_pts, imagePoints, mtx, dist, flags=cv2.SOLVEPNP_ITERATIVE)
+             #   imgpts, jac = cv2.projectPoints(opoints, prvecs, ptvecs, mtx, dcoeffs)
+
+                # Draws the edges of the pose onto the image
+                #print(str(prvecs) + '\n' + str(ptvecs) + '\n\n\n')
+                apriltags_found+=1
+                os.system('clear')
+                print(f'pitch:{prvecs[0]}')
+                print(f'yaw:{prvecs[1]}')
+                print(f'roll:{prvecs[2]}') #
+
+                print(f'left right:{-ptvecs[0]-ptvecs[2]/4}')
+                print(f'up down:{(ptvecs[1]+ptvecs[2]/16)*2}')
+                print(f'distance:{ptvecs[2]}')
+                print()
+                #sys.stdout.write("\a")
+                data.append([prvecs[0],
+                             prvecs[1],
+                             prvecs[2],
+                             -ptvecs[0]-ptvecs[2]/4,
+                             (ptvecs[1]+ptvecs[2]/16)*2,
+                             ptvecs[2]])
+    #time.sleep(0.1)
+pitch=[float(i[0]) for i in data]
+yaw=[float(i[1]) for i in data]
+roll=[float(i[2]) for i in data]
+left_right=[float(i[3]) for i in data]
+up_down=[float(i[4]) for i in data]
+distance=[float(i[5]) for i in data]
+
+f=open('data.json','r')
+data=json.loads(f.read())
+f.close()
+data.append([
+    [np.average(pitch),np.average(yaw),np.average(left_right),np.average(up_down),np.average(distance)],
+    [np.std(pitch),np.std(yaw),np.std(left_right),np.std(up_down),np.std(distance)]
+])
+f=open('data.json','w')
+f.write(json.dumps(data))
+f.close()
