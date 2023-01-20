be9ff03334a2080d9dd0dd86cd6923006234b9cc
Colin <cfinn68916@gmail.com>
2023-01-20 16:07:18 -0600

    add documentation

--- test/Camera_Tests/apriltagModule/__init__.py
+++ test/Camera_Tests/apriltagModule/__init__.py
@@ -1,36 +1,39 @@
-'''
-TODO: add detection class
-TODO: make getPosition return
-
-'''
-
-import math, time, sys, json
+import math
 import apriltag
 import cv2
 import numpy as np
 
 tag_size = 8 / 2.54
 
-roll_threshold = 0.3
-
 
 class Detection:
-    def __init__(self, yaw, pitch, roll, left_right, up_down, distance, RMSError, tagID):
+    def __init__(self, yaw, pitch, roll, left_right, up_down, distance, rms_error, tag_id):
         self.yaw = yaw
         self.pitch = pitch
         self.roll = roll
         self.left_right = left_right
         self.up_down = up_down
         self.distance = distance
-        self.RMSError = RMSError
-        self.tagID = tagID
+        self.RMSError = rms_error
+        self.tagID = tag_id
 
 
 def convert(pt):
     return float(pt[0]), float(pt[1])
 
 
-def getPosition(img, camera_mtx, dist_coefficients):
+def getPosition(img, camera_mtx, dist_coefficients, tags=range(9), roll_threshold=0.3):
+    """
+    This function takes an image and returns the position of apriltags in the image
+
+    :param img: The image you want to find apriltags in
+    :param camera_mtx: The camera's calibration matrix
+    :param dist_coefficients: The distortion coefficients of the camera
+    :param tags: (Default: 1-9) The apriltags to look for
+    :param roll_threshold: (Default: 0.3 radians/17 degrees) The maximum roll of the apriltag. Helps remove false detections.
+    :return: A list of Detection objects, or None if it fails
+    :rtype: list(Detection objects)
+    """
     # Convert Images to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -45,25 +48,19 @@ def getPosition(img, camera_mtx, dist_coefficients):
                                        refine_pose=True,
                                        debug=False,
                                        quad_contours=True)
-
+    # Create a detector with given options
     detector = apriltag.Detector(options)
-
+    # Find the apriltags
     detection_results = detector.detect(gray)
 
-    num_detections = len(detection_results)
-
-    img_points_arr = []
-    obj_points_arr = []
-    points_array = []
-    detections=[]
-    if num_detections > 0:
-        for i, detection in enumerate(detection_results):
-            if detection.tag_id > 8:
+    detections = []
+    if len(detection_results) > 0: # Check if there are any apriltags
+        for detection in detection_results:
+            # Check if apriltag is allowed
+            if detection.tag_id not in tags:
                 continue
 
-            imagePoints = detection.corners.reshape(1, 4, 2)
-
-            # Get the tag size from .json file
+            image_points = detection.corners.reshape(1, 4, 2)
 
             ob_pt1 = [-tag_size / 2, -tag_size / 2, 0.0]
             ob_pt2 = [tag_size / 2, -tag_size / 2, 0.0]
@@ -72,39 +69,26 @@ def getPosition(img, camera_mtx, dist_coefficients):
             ob_pts = ob_pt1 + ob_pt2 + ob_pt3 + ob_pt4
             object_pts = np.array(ob_pts).reshape(4, 3)
 
-            opoints = np.array([
-                -1, -1, 0,
-                1, -1, 0,
-                1, 1, 0,
-                -1, 1, 0,
-                -1, -1, -2 * 1,
-                1, -1, -2 * 1,
-                1, 1, -2 * 1,
-                -1, 1, -2 * 1,
-            ]).reshape(-1, 1, 3) * 0.5 * tag_size
-
-            img_points_arr.append(imagePoints)
-            obj_points_arr.append(object_pts)
-            points_array.append(opoints)
-
-            # mtx - the camera calibration's intrinsics
-            good, prvecs, ptvecs, RMS = cv2.solvePnPGeneric(object_pts, imagePoints, camera_mtx, dist_coefficients,
-                                                flags=cv2.SOLVEPNP_ITERATIVE)
+            # Solve for rotation and translation
+            good, rotation_vector, translation_vector, rms = cv2.solvePnPGeneric(object_pts, image_points, camera_mtx,
+                                                                                 dist_coefficients,
+                                                                                 flags=cv2.SOLVEPNP_ITERATIVE)
             assert good, 'something went wrong with solvePnP'
-            # Draws the edges of the pose onto the image
-            pitch = prvecs[0][0]
-            yaw = prvecs[0][1]
-            roll = prvecs[0][2]
 
-            left_right = -ptvecs[0][0] - ptvecs[0][2] / 4
-            up_down = (ptvecs[0][1] + ptvecs[0][2] / 16) * 2
-            distance = ptvecs[0][2]
+            # Map rotation_vector
+            pitch, yaw, roll = rotation_vector[0]
 
+            left_right = -translation_vector[0][0] - translation_vector[0][2] / 4
+            up_down = (translation_vector[0][1] + translation_vector[0][2] / 16) * 2
+            distance = translation_vector[0][2]
+
+            # Check if roll is within limit
             if math.fabs(roll) > roll_threshold:
                 print('discarded a value')
                 continue
-            detections.append(Detection(yaw, pitch, roll, left_right, up_down, distance, RMS,
-                             detection.tag_id))
+            detections.append(Detection(yaw, pitch, roll, left_right, up_down, distance, rms,
+                                        detection.tag_id))
     else:
+        # If there are no apriltags, return None
         return None
     return detections
