b673e28e01928bacda72b2f7eda3105f72507bb5
MD33 <manningisaac4@gmail.com>
2023-02-02 17:34:41 -0600

    Added basic flask server

--- test/nt-drive/rr1716/DEATHSTARE.py
+++ test/nt-drive/rr1716/DEATHSTARE.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python
 
 import logging
+from flask import Flask, render_template, Response, redirect
+from threading import Thread
 
-class TestClass:
-    def __init__(self):
-        pass
+app = Flask(__name__)
 
-    def print_string(self, string):
-        print(string)
+# This function gets called by the /video_feed route below
+def gen_frames(app):  # generate frame by frame from camera
+
+    # We want to loop this forever
+    while True:
+
+        frame = app.open_cv.get_jpg_bytes()
+        # Return the image to the browser
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 
-if __name__ == "__main__":
-    # We're a module, never run anything here
+@app.route('/video_feed')
+def video_feed():
+    #Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(app), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+def start(camera):
+    app.open_cv = camera
+    thread = Thread(target=app.run)
+    thread.start()
+
+if __name__ == '__main__':
     pass
 else:
-    # Run things on import here
-    
     pass
