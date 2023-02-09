3c2799ed182a8f4f87f27623ba0dae07dc042fec
glde <92763974+gldeA@users.noreply.github.com>
2023-02-09 16:50:58 -0600

    Added a flag to show when the robot is running or not

--- robot/src/main/cpp/Robot.cpp
+++ robot/src/main/cpp/Robot.cpp
@@ -2,6 +2,11 @@
 
 #include <frc/smartdashboard/SmartDashboard.h>
 #include <frc2/command/CommandScheduler.h>
+#include <frc/shuffleboard/Shuffleboard.h>
+
+void Robot::RobotInit() {
+	robotRunning = frc::Shuffleboard::GetTab("Controller").Add("Robot Running", false).GetEntry();
+}
 
 void Robot::RobotPeriodic() {
 	frc2::CommandScheduler::GetInstance().Run();
@@ -13,6 +18,8 @@ void Robot::AutonomousInit() {
 	if (autonomousCommand != nullptr) {
 		autonomousCommand->Schedule();
 	}
+
+	robotRunning->SetBoolean(true);
 }
 
 void Robot::TeleopInit() {
@@ -20,10 +27,16 @@ void Robot::TeleopInit() {
 		autonomousCommand->Cancel();
     	autonomousCommand = nullptr;
 	}
+
+	robotRunning->SetBoolean(true);
 }
 
 void Robot::TeleopPeriodic() {
 	robotContainer.UpdateControllerLogging();
+}
+
+void Robot::DisabledInit() {
+	robotRunning->SetBoolean(false);
 }
 
 #ifndef RUNNING_FRC_TESTS
--- robot/src/main/include/Robot.h
+++ robot/src/main/include/Robot.h
@@ -7,12 +7,16 @@
 
 class Robot : public frc::TimedRobot {
  public:
+	void RobotInit() override;
 	void RobotPeriodic() override;
 	void AutonomousInit() override;
 	void TeleopInit() override;
 	void TeleopPeriodic() override;
+	void DisabledInit() override;
 	
 private:
 	RobotContainer robotContainer;
 	frc2::Command* autonomousCommand = nullptr;
+
+	nt::GenericEntry* robotRunning;
 };
