1dfdf31eb3ea7e33e9928a08345e53837ece77ae
glde <92763974+gldeA@users.noreply.github.com>
2023-02-13 16:26:40 -0600

    Added `1716GameInfo` value functionality for networktables

--- robot/src/main/cpp/Robot.cpp
+++ robot/src/main/cpp/Robot.cpp
@@ -3,8 +3,25 @@
 #include <frc/smartdashboard/SmartDashboard.h>
 #include <frc2/command/CommandScheduler.h>
 
+void Robot::RobotInit() {
+	networkTableInst = nt::NetworkTableInstance::GetDefault();
+	table = networkTableInst.GetTable("1716GameInfo");
+
+	isAutonomous = table->GetBooleanTopic("isAutonomous").Publish();
+	isTeleop = table->GetBooleanTopic("isTeleop").Publish();
+	isRedAlliance = table->GetBooleanTopic("isRedAlliance").Publish();
+	matchTime = table->GetDoubleTopic("matchTime").Publish();
+
+	isRedAlliance.Set(driverStation.GetAlliance() == frc::DriverStation::Alliance::kRed);
+	isAutonomous.Set(false);
+	isTeleop.Set(false);
+	matchTime.Set(0);
+}
+
 void Robot::RobotPeriodic() {
 	frc2::CommandScheduler::GetInstance().Run();
+
+	matchTime.Set(driverStation.GetMatchTime());
 }
 
 void Robot::AutonomousInit() {
@@ -13,6 +30,8 @@ void Robot::AutonomousInit() {
 	if (autonomousCommand != nullptr) {
 		autonomousCommand->Schedule();
 	}
+
+	isAutonomous.Set(true);
 }
 
 void Robot::TeleopInit() {
@@ -20,6 +39,13 @@ void Robot::TeleopInit() {
 		autonomousCommand->Cancel();
     	autonomousCommand = nullptr;
 	}
+
+	isAutonomous.Set(false);
+	isTeleop.Set(true);
+}
+
+void Robot::DisabledInit() {
+	isTeleop.Set(false);
 }
 
 #ifndef RUNNING_FRC_TESTS
--- robot/src/main/include/Robot.h
+++ robot/src/main/include/Robot.h
@@ -2,17 +2,31 @@
 
 #include <frc/TimedRobot.h>
 #include <frc2/command/Command.h>
-// #include <RobotContainer.h>
+#include <frc/DriverStation.h>
+#include <networktables/BooleanTopic.h>
+#include <networktables/DoubleTopic.h>
 
 #include "RobotContainer.h"
 
 class Robot : public frc::TimedRobot {
- public:
+public:
+ 	void RobotInit() override;
 	void RobotPeriodic() override;
 	void AutonomousInit() override;
 	void TeleopInit() override;
+	void DisabledInit() override;
 	
 private:
 	RobotContainer robotContainer;
 	frc2::Command* autonomousCommand = nullptr;
+
+	frc::DriverStation driverStation;
+
+	nt::NetworkTableInstance networkTableInst;
+	std::shared_ptr<nt::NetworkTable> table;
+
+	nt::BooleanPublisher isAutonomous;
+	nt::BooleanPublisher isTeleop;
+	nt::BooleanPublisher isRedAlliance;
+	nt::DoublePublisher matchTime;
 };
