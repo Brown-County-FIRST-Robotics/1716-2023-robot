ed775bc55952fbc156e03d058d8e7421dcf48dd7
Colin <cfinn68916@gmail.com>
2023-02-15 15:43:32 -0600

    Add logging to NetworkTables1716.py

--- test/nt-drive/rr1716/NetworkTables1716.py
+++ test/nt-drive/rr1716/NetworkTables1716.py
@@ -8,56 +8,72 @@ IP = "10.17.16.2"
 
 class NetworkTablesWrapper:
     def __init__(self):
+        logging.debug('NetworkTablesWrapper.init')
         self.drive_table = NetworkTables.getTable('1716Drive')
         self.pigeon_table = NetworkTables.getTable('1716Pigeon')
         self.game_table = NetworkTables.getTable('1716GameInfo')
         self.encoder_table = NetworkTables.getTable('1716Encoder')
 
     def Drive(self, x, y, r):
+        logging.debug(f'NetworkTablesWrapper.Drive({x},{y},{r})')
         self.drive_table.putNumber("x", x)
         self.drive_table.putNumber("y", y)
         self.drive_table.putNumber("rotation", r)
 
     def SwitchToTank(self):
+        logging.debug(f'NetworkTablesWrapper.SwitchToTank()')
         self.drive_table.putBoolean('isTank', True)
 
     def SwitchToMecanum(self):
+        logging.debug(f'NetworkTablesWrapper.SwitchToMecanum()')
         self.drive_table.putBoolean('isTank', False)
 
     def StartAutoBalance(self):
+        logging.debug(f'NetworkTablesWrapper.StartAutoBalance()')
         self.drive_table.putBoolean('startAutoBalance', True)
 
     def EndAutoBalance(self):
+        logging.debug(f'NetworkTablesWrapper.EndAutoBalance()')
         self.drive_table.putBoolean('startAutoBalance', False)
 
     def SetArmPortal(self):  # TODO:rename
+        logging.debug(f'NetworkTablesWrapper.SetArmPortal()')
         self.drive_table.putBoolean('setArmPortal', True)
 
     def SetArmFloor(self):  # TODO:rename
+        logging.debug(f'NetworkTablesWrapper.SetArmFloor()')
         self.drive_table.putBoolean('setArmFloor', True)
 
     def PickupObject(self):
+        logging.debug(f'NetworkTablesWrapper.PickupObject()')
         self.drive_table.putBoolean('pickupObject', True)
 
     def SetArmLow(self):  # TODO:rename
+        logging.debug(f'NetworkTablesWrapper.SetArmLow()')
         self.drive_table.putBoolean('setArmLow', True)
 
     def SetArmMedium(self):  # TODO:rename
+        logging.debug(f'NetworkTablesWrapper.SetArmMedium()')
         self.drive_table.putBoolean('setArmMedium', True)
 
     def SetArmHigh(self):  # TODO:rename
+        logging.debug(f'NetworkTablesWrapper.SetArmHigh()')
         self.drive_table.putBoolean('setArmHigh', True)
 
     def SetArmHighNode(self):  # TODO:rename
+        logging.debug(f'NetworkTablesWrapper.SetArmHighNode()')
         self.drive_table.putBoolean('setArmHighNode', True)
 
     def SetArmLowNode(self):  # TODO:rename
+        logging.debug(f'NetworkTablesWrapper.SetArmLowNode()')
         self.drive_table.putBoolean('setArmLowNode', True)
 
     def RetractArm(self):
+        logging.debug(f'NetworkTablesWrapper.RetractArm()')
         self.drive_table.putBoolean('retractArm', True)
 
     def IsArmDone(self):
+        logging.debug(f'NetworkTablesWrapper.IsArmDone()')
         arm_values = [
             self.drive_table.getBoolean('setArmLowNode', False),
             self.drive_table.getBoolean('setArmHighNode', False),
@@ -70,9 +86,11 @@ class NetworkTablesWrapper:
         return True not in arm_values
 
     def DropObject(self):
+        logging.debug(f'NetworkTablesWrapper.DropObject()')
         self.drive_table.putBoolean('dropObject', True)
 
     def GetAccel(self):
+        logging.debug(f'NetworkTablesWrapper.GetAccel()')
         x_accel = self.pigeon_table.getNumber('xAccel', -1)
         y_accel = self.pigeon_table.getNumber('yAccel', -1)
         z_accel = self.pigeon_table.getNumber('zAccel', -1)
@@ -82,6 +100,7 @@ class NetworkTablesWrapper:
         return x_accel, y_accel, z_accel
 
     def GetYaw(self):
+        logging.debug(f'NetworkTablesWrapper.GetYaw()')
         yaw = self.pigeon_table.getNumber('yaw', -1)
         if yaw == -1:
             print('No yaw value')
@@ -89,18 +108,22 @@ class NetworkTablesWrapper:
         return yaw
 
     def IsAutonomous(self):  # TODO:add tests
+        logging.debug(f'NetworkTablesWrapper.IsAutonomous()')
         val = self.game_table.getBoolean('isAutonomous', False)
         return val
 
     def IsTeleop(self):  # TODO:add tests
+        logging.debug(f'NetworkTablesWrapper.IsTeleop()')
         val = self.game_table.getBoolean('isTeleop', False)
         return val
 
     def IsRedAlliance(self):  # TODO:add tests
+        logging.debug(f'NetworkTablesWrapper.IsRedAlliance()')
         val = self.game_table.getBoolean('isRedAlliance', False)
         return val
 
     def GetMatchTime(self):  # TODO:add tests
+        logging.debug(f'NetworkTablesWrapper.GetMatchTime()')
         match_time = self.pigeon_table.getNumber('matchTime', -1)
         if match_time == -1:
             print('No match_time value')
@@ -108,6 +131,7 @@ class NetworkTablesWrapper:
         return match_time
 
     def GetEncoderVals(self):
+        logging.debug(f'NetworkTablesWrapper.GetEncoderVals()')
         encoder_values = [
             self.encoder_table.getNumber('frontLeftEncoder', '-1'),
             self.encoder_table.getNumber('backLeftEncoder', '-1'),
@@ -120,6 +144,7 @@ class NetworkTablesWrapper:
         return encoder_values
 
     def ResetEncoderVals(self):
+        logging.debug(f'NetworkTablesWrapper.ResetEncoderVals')
         self.encoder_table.putBoolean('encoderReset', True)
 
 
