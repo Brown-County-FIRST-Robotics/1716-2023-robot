dc4d7c43c9708dfb615e2af07c3c8e42342e050a
Colin <cfinn68916@gmail.com>
2023-02-15 15:36:29 -0600

    Add concurrency to apriltags in Decision.py

--- test/nt-drive/rr1716/Decision.py
+++ test/nt-drive/rr1716/Decision.py
@@ -7,19 +7,26 @@ import cv2
 import numpy as np
 # from rr1716 import Filter
 
+
 class Action:
-    def __init__(self, filter, cams, nt_interface, referrer):
+    def __init__(self, filter, cams, nt_interface, april_executor, referrer):
         self.referrer = referrer
         self.nt_interface = nt_interface
         self.cams = cams
         self.filter = filter
+        self.april_executor = april_executor
+
 
     def GetFilter(self):
         robotLocation = None
-        for camera in self.cams:
-            res = AprilTags.getPosition(camera.get_gray(), camera.camera_matrix, None)  # check for apriltag
-            if res is not None and res != []:  # TODO: change?
-                robotLocation = res[0]  # TODO: change?
+        april_futures = []
+
+        for cam in self.cams:
+            april_futures.append(self.april_executor.submit(AprilTags.getPosition, cam.frame, cam.camera_matrix, None))
+        for future in april_futures:
+            detections = future.result()
+            if detections is not None:
+                robotLocation=detections[0]
                 break
         if robotLocation is not None:
             self.filter.updateWithApriltag(robotLocation, self.nt_interface)
@@ -42,8 +49,8 @@ class Action:
 
 
 class StartFilter(Action):
-    def __init__(self, filter, cams, nt_interface, referrer):
-        super().__init__(filter, cams, nt_interface, referrer)
+    def __init__(self, filter, cams, nt_interface, april_executor, referrer):
+        super().__init__(filter, cams, nt_interface, april_executor, referrer)
 
     def Step(self):
         robotLocation = None
@@ -59,12 +66,12 @@ class StartFilter(Action):
         return None not in self.filter.lastApril  # check if lastApril has a None, meaning it has not seen an apriltag
 
     def MakeChild(self):
-        return AsyncSetHeight(self.filter, self.cams, self.nt_interface, 6, self.referrer)  # IMPORTANT: change
+        return AsyncSetHeight(self.filter, self.cams, self.nt_interface, self.april_executor, self.referrer, 6)  # IMPORTANT: change
 
 
 class AsyncSetHeight(Action):
-    def __init__(self, filter, cams, nt_interface, referrer, height):
-        super().__init__(filter, cams, nt_interface, referrer)
+    def __init__(self, filter, cams, nt_interface, april_executor, referrer, height):
+        super().__init__(filter, cams, nt_interface, april_executor, referrer)
         '''
         0:Floor
         1:portal
@@ -92,18 +99,18 @@ class AsyncSetHeight(Action):
 
     def MakeChild(self):
         if self.referrer == 'auto':
-            return DriveToLocation(self.filter, self.cams, self.nt_interface, [0, 0, 0], self.referrer)
+            return DriveToLocation(self.filter, self.cams, self.nt_interface,self.april_executor, [0, 0, 0], self.referrer)
 
 
 class DriveToLocation(Action):
-    def __init__(self, filter, cams, nt_interface, location, referrer):
-        super().__init__(filter, cams, nt_interface, referrer)
+    def __init__(self, filter, cams, nt_interface, april_executor, location, referrer):
+        super().__init__(filter, cams, nt_interface, april_executor, referrer)
         self.location = location
 
     def Step(self):
         field_x, field_y, field_r = self.GetFilter()
-        cx = math.cos((field_r) * math.pi / 180)
-        cy = math.sin((field_r) * math.pi / 180)
+        cx = math.cos(field_r * math.pi / 180)
+        cy = math.sin(field_r * math.pi / 180)
 
         ax = math.cos((field_r + 90) * math.pi / 180)
         ay = math.sin((field_r + 90) * math.pi / 180)
@@ -126,35 +133,35 @@ class DriveToLocation(Action):
 
     def MakeChild(self):
         if self.referrer == 'auto':
-            return AwaitSetHeight(self.filter, self.cams, self.nt_interface, self.referrer)
+            return AwaitSetHeight(self.filter, self.cams, self.nt_interface,self.april_executor, self.referrer)
 
 
 class AwaitSetHeight(Action):
-    def __init__(self, filter, cams, nt_interface, referrer):
-        super().__init__(filter, cams, nt_interface, referrer)
+    def __init__(self, filter, cams, nt_interface, april_executor, referrer):
+        super().__init__(filter, cams, nt_interface, april_executor, referrer)
 
     def ShouldEnd(self):
         return self.nt_interface.IsArmDone()
 
     def MakeChild(self):
         if self.referrer == "auto":
-            return Drop(self.filter, self.cams, self.nt_interface, self.referrer)  # IMPORTANT: change
+            return Drop(self.filter, self.cams, self.nt_interface,self.april_executor, self.referrer)  # IMPORTANT: change
 
 
 class Drop(Action):
-    def __init__(self, filter, cams, nt_interface, referrer):
-        super().__init__(filter, cams, nt_interface, referrer)
+    def __init__(self, filter, cams, nt_interface, april_executor, referrer):
+        super().__init__(filter, cams, nt_interface, april_executor, referrer)
         self.nt_interface.DropObject()
         self.nt_interface.RetractArm()
 
     def MakeChild(self):
         if self.referrer == "auto":
-            return GetOnStation(self.filter, self.cams, self.nt_interface, self.referrer)  # IMPORTANT: change
+            return GetOnStation(self.filter, self.cams, self.nt_interface,self.april_executor, self.referrer)  # IMPORTANT: change
 
 
 class GetOnStation(Action):
-    def __init__(self, filter, cams, nt_interface, referrer):
-        super().__init__(filter, cams, nt_interface, referrer)
+    def __init__(self, filter, cams, nt_interface, april_executor, referrer):
+        super().__init__(filter, cams, nt_interface, april_executor, referrer)
         self.nt_interface.SwitchToTank()
         self.nt_interface.Drive(0, -1, 0)
 
@@ -166,16 +173,16 @@ class GetOnStation(Action):
         self.nt_interface.Drive(0, 0, 0)
 
     def MakeChild(self):
-        return AutoBalance(self.filter, self.cams, self.nt_interface, self.referrer)
+        return AutoBalance(self.filter, self.cams, self.nt_interface,self.april_executor, self.referrer)
 
 
 class AutoBalance(Action):
-    def __init__(self, filter, cams, nt_interface, referrer):
-        super().__init__(filter, cams, nt_interface, referrer)
+    def __init__(self, filter, cams, nt_interface, april_executor, referrer):
+        super().__init__(filter, cams, nt_interface, april_executor, referrer)
         self.nt_interface.StartAutoBalance()
 
     def ShouldEnd(self):
-        return self.nt_interface.IsTeleop()
+        return self.nt_interface.IsTeleop() and self.referrer=='auto'
 
     def End(self):
         self.nt_interface.EndAutoBalance()
@@ -183,7 +190,7 @@ class AutoBalance(Action):
 
     def MakeChild(self):
         pass  # if self.referrer=="auto":
-        # return GetOnStation(self.filter, self.cams, self.nt_interface, self.referrer)  # IMPORTANT: change
+        # return GetOnStation(self.filter, self.cams, self.nt_interface,self.april_executor, self.referrer)  # IMPORTANT: change
 
 
 def doCurrentAction(action):
