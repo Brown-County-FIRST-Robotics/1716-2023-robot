aaaa5faca6d32d774212b5c3c9b8a4f711f0c905
Colin Finn <56322557+cfinn68916@users.noreply.github.com>
2023-03-02 08:49:13 -0600

    Update Strategy.py

--- test/nt-drive/rr1716/Strategy.py
+++ test/nt-drive/rr1716/Strategy.py
@@ -3,6 +3,6 @@ r_p_factor = 0.001  # TODO: change
 
 r_error_factor = 0.75  # TODO: change
 
-drive_error_threshold = 1  # TODO: change
+drive_error_threshold = 10  # TODO: change
 
 accel_angle_threshold = 10  # TODO: change
