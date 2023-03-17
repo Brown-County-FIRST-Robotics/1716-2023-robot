47005bcbb4ba3318478c9354cbe2efb486002eab
Colin <cfinn68916@gmail.com>
2023-03-17 10:35:16 -0500

    Add --make_config

--- test/nt-drive/configs.py
+++ test/nt-drive/configs.py
@@ -43,3 +43,42 @@ if __name__ == '__main__':
                 print('Config file written')
         else:
             print('Aborted')
+
+    if args.make_config:
+        print('Making config file')
+        config = []
+        for i in range(int(input('Number of cameras:'))):
+            config.append({})
+            while True:
+                port=input(f'Camera {i+1} port:')
+                if not os.path.exists(port):
+                    print('Camera path does not exist')
+                else:
+                    config[-1]['port']=port
+                    break
+            while True:
+                cam_config=input('Camera config file:')
+                if os.path.exists(cam_config):
+                    config[-1]['config']=cam_config
+                    break
+                else:
+                    print('Camera config does not exist')
+            while True:
+                with open(config[-1]['config'], 'r') as f:
+                    if json.loads(f.read())['role'] in ['*', 'apriltag']:
+                        pos=input('Camera position (x,y,r):')
+                        if pos=='':
+                            pos='[0,0,0]'
+                        pos=json.loads(pos)
+                        if type(pos)==list and len(pos)==3:
+                            config[-1]['pos']=pos
+                            break
+                    else:
+                        print('This camera does not need a position')
+                        config[-1]['pos']=None
+        config_fname = input('Name of config file:')
+        assert input(
+            f'This will create a file called "configs/{config_fname}.json", are you sure(y/n):') == 'y', 'Operation canceled'
+        with open(f'configs/{config_fname}.json', 'w') as config_file:
+            config_file.write(json.dumps(config))
+        print('Config file made')
