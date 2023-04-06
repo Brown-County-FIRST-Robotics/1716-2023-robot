07fe9be813a14f879b948aeece249f97ed9333d7
Colin <cfinn68916@gmail.com>
2023-04-05 21:11:27 -0500

    Move to function

--- test/nt-drive/sim/render.py
+++ test/nt-drive/sim/render.py
@@ -1,73 +1,83 @@
 import cv2
 import numpy as np
 
-
-# TODO: turn into function
-# Generate a black image
-img = np.zeros((480, 640, 3), dtype=np.uint8)
-img[:, :] = (255,0,20)
-k=1
-# Define the rectangle corners in 3D space
-pixel_pos=[
-    [[-4*k,3*k,0],[-4*k,4*k,0],[4*k,4*k,0],[4*k,3*k,0]],
-    [[-4*k,-3*k,0],[-4*k,-4*k,0],[4*k,-4*k,0],[4*k,-3*k,0]],
-    [[-4*k,-3*k,0],[-3*k,-3*k,0],[-3*k,3*k,0],[-4*k,3*k,0]],
-    [[4*k,-3*k,0],[3*k,-3*k,0],[3*k,3*k,0],[4*k,3*k,0]],
-    [[-3*k,2*k,0],[-3*k,3*k,0],[3*k,3*k,0],[3*k,2*k,0]],
-    [[-3*k,-2*k,0],[-3*k,-3*k,0],[3*k,-3*k,0],[3*k,-2*k,0]],
-    [[-3*k,-2*k,0],[-2*k,-2*k,0],[-2*k,2*k,0],[-3*k,2*k,0]],
-    [[3*k,-2*k,0],[2*k,-2*k,0],[2*k,2*k,0],[3*k,2*k,0]],
-
-    [[-2*k,-2*k,0],[-2*k,-1*k,0],[-1*k,-1*k,0],[-1*k,-2*k,0]],
-    [[-1*k,-2*k,0],[-1*k,-1*k,0],[0*k,-1*k,0],[0*k,-2*k,0]],
-    [[0*k,-2*k,0],[0*k,-1*k,0],[1*k,-1*k,0],[1*k,-2*k,0]],
-    [[1*k,-2*k,0],[1*k,-1*k,0],[2*k,-1*k,0],[2*k,-2*k,0]],
-
-    [[-2*k,-1*k,0],[-2*k,0*k,0],[-1*k,0*k,0],[-1*k,-1*k,0]],
-    [[-1*k,-1*k,0],[-1*k,0*k,0],[0*k,0*k,0],[0*k,-1*k,0]],
-    [[0*k,-1*k,0],[0*k,0*k,0],[1*k,0*k,0],[1*k,-1*k,0]],
-    [[1*k,-1*k,0],[1*k,0*k,0],[2*k,0*k,0],[2*k,-1*k,0]],
-
-    [[-2*k,0*k,0],[-2*k,1*k,0],[-1*k,1*k,0],[-1*k,0*k,0]],
-    [[-1*k,0*k,0],[-1*k,1*k,0],[0*k,1*k,0],[0*k,0*k,0]],
-    [[0*k,0*k,0],[0*k,1*k,0],[1*k,1*k,0],[1*k,0*k,0]],
-    [[1*k,0*k,0],[1*k,1*k,0],[2*k,1*k,0],[2*k,0*k,0]],
-
-    [[-2*k,1*k,0],[-2*k,2*k,0],[-1*k,2*k,0],[-1*k,1*k,0]],
-    [[-1*k,1*k,0],[-1*k,2*k,0],[0*k,2*k,0],[0*k,1*k,0]],
-    [[0*k,1*k,0],[0*k,2*k,0],[1*k,2*k,0],[1*k,1*k,0]],
-    [[1*k,1*k,0],[1*k,2*k,0],[2*k,2*k,0],[2*k,1*k,0]],
-]
-pixel_val=[
-    1,1,1,1, # outer deadzone
-    0,0,0,0, # inner deadzone
-
-    0,0,1,0,
-    1,1,1,0,
-    1,0,1,0,
-    0,1,0,1
+apriltags=[
+    [0,0,1,0,1,1,1,0,1,0,1,0,0,1,0,1],
+    [0,0,1,1,0,1,0,0,0,1,1,0,1,0,1,0],
+    [0,1,0,0,0,1,0,1,1,0,1,1,1,0,0,1],
+    [0,1,1,1,1,0,0,1,1,0,1,0,0,1,1,0],
+    [0,1,1,1,1,1,1,1,0,1,1,0,1,0,1,1],
+    [1,0,1,1,0,0,1,1,0,1,0,1,1,0,0,0],
+    [1,1,1,0,0,1,1,1,0,1,0,0,0,1,0,1],
+    [1,1,1,1,1,1,1,0,0,1,0,1,1,0,0,1]
 ]
 
-# Project the rectangle corners onto the image plane
-focal_length = 500
-principal_point = (320, 240)
-camera_matrix = np.array([
-    [focal_length, 0, principal_point[0]],
-    [0, focal_length, principal_point[1]],
-    [0, 0, 1]
-], dtype=np.float32)
 
-rotation_vector = np.array([40, 0, 0], dtype=np.float32) # pitch,yaw,roll
-translation_vector = np.array([4, 3, 30], dtype=np.float32) # lr,ud,dist
-for pts,color in zip(pixel_pos,pixel_val):
-    projected_points, _ = cv2.projectPoints(np.array([np.array(i) for i in pts], dtype=np.float32), np.deg2rad(rotation_vector), translation_vector, camera_matrix, None)
+def genImage(tvec,rvec,tagid,size):
+    # Generate a black image
+    img = np.zeros(tuple(size + tuple([3])), dtype=np.uint8)
+    img[:, :] = (255,0,20)
+    k=1
+    # Define the rectangle corners in 3D space
+    pixel_pos=[
+        [[-4*k,3*k,0],[-4*k,4*k,0],[4*k,4*k,0],[4*k,3*k,0]],
+        [[-4*k,-3*k,0],[-4*k,-4*k,0],[4*k,-4*k,0],[4*k,-3*k,0]],
+        [[-4*k,-3*k,0],[-3*k,-3*k,0],[-3*k,3*k,0],[-4*k,3*k,0]],
+        [[4*k,-3*k,0],[3*k,-3*k,0],[3*k,3*k,0],[4*k,3*k,0]],
+        [[-3*k,2*k,0],[-3*k,3*k,0],[3*k,3*k,0],[3*k,2*k,0]],
+        [[-3*k,-2*k,0],[-3*k,-3*k,0],[3*k,-3*k,0],[3*k,-2*k,0]],
+        [[-3*k,-2*k,0],[-2*k,-2*k,0],[-2*k,2*k,0],[-3*k,2*k,0]],
+        [[3*k,-2*k,0],[2*k,-2*k,0],[2*k,2*k,0],[3*k,2*k,0]],
 
-    # Draw the rectangle on the image
-    points = projected_points.astype(np.int32).reshape((-1, 2))
-    cv2.fillConvexPoly(img, points, (255, 255, 255) if color else (0,0,0))
+        [[-2*k,-2*k,0],[-2*k,-1*k,0],[-1*k,-1*k,0],[-1*k,-2*k,0]],
+        [[-1*k,-2*k,0],[-1*k,-1*k,0],[0*k,-1*k,0],[0*k,-2*k,0]],
+        [[0*k,-2*k,0],[0*k,-1*k,0],[1*k,-1*k,0],[1*k,-2*k,0]],
+        [[1*k,-2*k,0],[1*k,-1*k,0],[2*k,-1*k,0],[2*k,-2*k,0]],
+
+        [[-2*k,-1*k,0],[-2*k,0*k,0],[-1*k,0*k,0],[-1*k,-1*k,0]],
+        [[-1*k,-1*k,0],[-1*k,0*k,0],[0*k,0*k,0],[0*k,-1*k,0]],
+        [[0*k,-1*k,0],[0*k,0*k,0],[1*k,0*k,0],[1*k,-1*k,0]],
+        [[1*k,-1*k,0],[1*k,0*k,0],[2*k,0*k,0],[2*k,-1*k,0]],
+
+        [[-2*k,0*k,0],[-2*k,1*k,0],[-1*k,1*k,0],[-1*k,0*k,0]],
+        [[-1*k,0*k,0],[-1*k,1*k,0],[0*k,1*k,0],[0*k,0*k,0]],
+        [[0*k,0*k,0],[0*k,1*k,0],[1*k,1*k,0],[1*k,0*k,0]],
+        [[1*k,0*k,0],[1*k,1*k,0],[2*k,1*k,0],[2*k,0*k,0]],
+
+        [[-2*k,1*k,0],[-2*k,2*k,0],[-1*k,2*k,0],[-1*k,1*k,0]],
+        [[-1*k,1*k,0],[-1*k,2*k,0],[0*k,2*k,0],[0*k,1*k,0]],
+        [[0*k,1*k,0],[0*k,2*k,0],[1*k,2*k,0],[1*k,1*k,0]],
+        [[1*k,1*k,0],[1*k,2*k,0],[2*k,2*k,0],[2*k,1*k,0]],
+    ]
+    pixel_val=[
+        1,1,1,1, # outer deadzone
+        0,0,0,0, # inner deadzone
+    ]+apriltags[tagid-1]
+
+    # Project the rectangle corners onto the image plane
+    focal_length = 500
+    principal_point = (size[1]/2,size[0]/2)
+    camera_matrix = np.array([
+        [focal_length, 0, principal_point[0]],
+        [0, focal_length, principal_point[1]],
+        [0, 0, 1]
+    ], dtype=np.float32)
+
+    # pitch,yaw,roll
+    # lr,ud,dist
+    rvec = np.array(rvec, dtype=np.float32)
+    tvec = np.array(tvec, dtype=np.float32)
+    for pts,color in zip(pixel_pos,pixel_val):
+        projected_points, _ = cv2.projectPoints(np.array([np.array(i) for i in pts], dtype=np.float32), np.deg2rad(rvec), tvec, camera_matrix, None)
+
+        # Draw the rectangle on the image
+        points = projected_points.astype(np.int32).reshape((-1, 2))
+        cv2.fillConvexPoly(img, points, (255, 255, 255) if color else (0,0,0))
+    return img
 
 # Save the image to a PNG file
 if __name__=="__main__":
+    img=genImage([0,0,10],[0,0,0],1, (480,640))
     cv2.imshow('Output Image', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
