e1e74f456ac034deb7c153b6533639874a34f529
Colin <cfinn68916@gmail.com>
2023-04-06 21:45:17 -0500

    Remove old decision code and switch to new web file format

--- test/nt-drive/rr1716/Web/DEATHSTARE.py
+++ test/nt-drive/rr1716/Web/DEATHSTARE.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import logging
-from flask import Flask, render_template, Response, redirect, request, jsonify, Blueprint, current_app
+from flask import Flask, render_template, Response, redirect, request, jsonify, Blueprint
 from threading import Thread
 from rr1716 import AprilTags
 import cv2
@@ -11,140 +11,138 @@ import dataclasses
 
 __COLOR_PICK_RANGE__ = 20
 
-ds_bp = Blueprint('DEATHSTARE', __name__)
-
 temp_cameras = []
-
-# This function gets called by the /video_feed route below
-def gen_frames(camera):  # generate frame by frame from camera
-    logging.debug("DEATHSTARE.gen_frames")
-    # We want to loop this forever
-    while True:
-        camera.clear_rectangle()
-        frame = camera.get_jpg_bytes()
-        # Return the image to the browser
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
-
-
-@ds_bp.route('/camera_feed/<int:Number>')
-def camera_feed(Number):
-    logging.debug("DEATHSTARE.camera_feed")
-
-    if len(current_app.Cameras) <= Number:
-        the_camera = current_app.Cameras[-1]
-    else:
-        the_camera = current_app.Cameras[Number]
-
-    # Video streaming route. Put this in the src attribute of an img tag
-    return Response(gen_frames(the_camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+def start(app):
+    # This function gets called by the /video_feed route below
+    def gen_frames(camera):  # generate frame by frame from camera
+        logging.debug("DEATHSTARE.gen_frames")
+        # We want to loop this forever
+        while True:
+            camera.clear_rectangle()
+            frame = camera.get_jpg_bytes()
+            # Return the image to the browser
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 
-@ds_bp.route('/return')
-def returnToIndex():
-    global current_app
-    logging.debug("DEATHSTARE.return")
-    for i in range(len(current_app.Cameras)):
-        current_app.Cameras[i].clear_rectangle()
-    """Video streaming home page."""
-    return redirect('/')
+    @app.route('/camera_feed/<int:Number>')
+    def camera_feed(Number):
+        logging.debug("DEATHSTARE.camera_feed")
 
-@ds_bp.route('/')
-def index():
-    logging.debug("DEATHSTARE.index")
-    """Video streaming home page."""
-    return render_template('sidecam.html')
+        if len(app.Cameras) <= Number:
+            the_camera = app.Cameras[-1]
+        else:
+            the_camera = app.Cameras[Number]
+
+        # Video streaming route. Put this in the src attribute of an img tag
+        return Response(gen_frames(the_camera), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-# remove this code for now TODO: add code
-@ds_bp.route('/calibrate')
-def calibrate():
-    logging.debug("DEATHSTARE.calibrate")
-    """Video streaming home page."""
-    return render_template('calibrate.html')
+    @app.route('/return')
+    def returnToIndex():
+        global app
+        logging.debug("DEATHSTARE.return")
+        for i in range(len(app.Cameras)):
+            app.Cameras[i].clear_rectangle()
+        """Video streaming home page."""
+        return redirect('/')
+
+    @app.route('/')
+    def index():
+        logging.debug("DEATHSTARE.index")
+        """Video streaming home page."""
+        return render_template('sidecam.html')
 
 
-#Color picker
-# This function gets called by the /video_feed route below
-def gen_frames_picker(camera):  # generate frame by frame from camera
-    logging.debug("DEATHSTARE.gen_frames_picker")
-    
-    w = camera.get_width()
-    h = camera.get_height()
-    x = int(w / 2)
-    y = int(h / 2) 
-    # We want to loop this forever
-    while True: 
-        camera.add_rectangle((x - __COLOR_PICK_RANGE__, y - __COLOR_PICK_RANGE__),
-                         (x + __COLOR_PICK_RANGE__, y + __COLOR_PICK_RANGE__),
-                         (0, 255, 0), 1)
-        frame = camera.get_jpg_bytes()
+    # remove this code for now TODO: add code
+    @app.route('/calibrate')
+    def calibrate():
+        logging.debug("DEATHSTARE.calibrate")
+        """Video streaming home page."""
+        return render_template('calibrate.html')
 
-        #print("dimensions", len(frame), len(frame[0]))
 
-        # Return the image to the browser
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+    #Color picker
+    # This function gets called by the /video_feed route below
+    def gen_frames_picker(camera):  # generate frame by frame from camera
+        logging.debug("DEATHSTARE.gen_frames_picker")
 
-@ds_bp.route('/picker_image')
-def picker_image():
-    logging.debug("DEATHSTARE.picker_image")
+        w = camera.get_width()
+        h = camera.get_height()
+        x = int(w / 2)
+        y = int(h / 2)
+        # We want to loop this forever
+        while True:
+            camera.add_rectangle((x - __COLOR_PICK_RANGE__, y - __COLOR_PICK_RANGE__),
+                             (x + __COLOR_PICK_RANGE__, y + __COLOR_PICK_RANGE__),
+                             (0, 255, 0), 1)
+            frame = camera.get_jpg_bytes()
 
-    the_camera = current_app.Cameras[0]
+            #print("dimensions", len(frame), len(frame[0]))
 
-    #Video streaming route. Put this in the src attribute of an img tag
-    return Response(gen_frames_picker(the_camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+            # Return the image to the browser
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
-@ds_bp.route('/picker')
-def picker_page():
-    logging.debug("DEATHSTARE.picker")
-    return render_template('picker.html')
+    @app.route('/picker_image')
+    def picker_image():
+        logging.debug("DEATHSTARE.picker_image")
 
-#Color picker
-@ds_bp.route('/preview')
-def preview_page():
-    logging.debug("DEATHSTARE.preview")
-    return render_template('preview.html')
+        the_camera = app.Cameras[0]
 
-@ds_bp.route('/preview_image')
-def preview_image():
-    logging.debug("DEATHSTARE.preview_image")
-    the_camera = current_app.Cameras[0]
-    #Video streaming route. Put this in the src attribute of an img tag
-    return Response(rr1716.Vision.gen_preview_picker(the_camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+        #Video streaming route. Put this in the src attribute of an img tag
+        return Response(gen_frames_picker(the_camera), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@ds_bp.route("/pick")
-def pick():
-    current_app.Cameras[0].get_frame()
-    col = rr1716.Vision.averageColor(current_app.Cameras[0].get_color(), __COLOR_PICK_RANGE__)
-    print("color: ", col) 
-    #write to file
-    file = open("picked_color", "w")
-    file.write(str(int(col[0])) + " " + str(int(col[1])) + " " + str(int(col[2])) + " \n")
-    file.close()
-    return redirect("/preview")
+    @app.route('/picker')
+    def picker_page():
+        logging.debug("DEATHSTARE.picker")
+        return render_template('picker.html')
 
-@ds_bp.route("/pick_cone")
-def pick_cone():
-    current_app.Cameras[0].get_frame()
-    col = rr1716.Vision.averageColor(current_app.Cameras[0].get_frame(), __COLOR_PICK_RANGE__)
-    logging.info("cone color: " + str(col)) 
-    #write to file
-    file = open("cone_picked_color", "w")
-    file.write(str(int(col[0])) + " " + str(int(col[1])) + " " + str(int(col[2])) + " \n")
-    file.close()
-    return redirect("/preview")
+    #Color picker
+    @app.route('/preview')
+    def preview_page():
+        logging.debug("DEATHSTARE.preview")
+        return render_template('preview.html')
 
-@ds_bp.route("/pick_cube")
-def pick_cube():
-    current_app.Cameras[0].get_frame()
-    col = rr1716.Vision.averageColor(current_app.Cameras[0].get_frame(), __COLOR_PICK_RANGE__)
-    logging.info("cube color: " + str(col)) 
-    #write to file
-    file = open("cube_picked_color", "w")
-    file.write(str(int(col[0])) + " " + str(int(col[1])) + " " + str(int(col[2])) + " \n")
-    file.close()
-    return redirect("/preview")
+    @app.route('/preview_image')
+    def preview_image():
+        logging.debug("DEATHSTARE.preview_image")
+        the_camera = app.Cameras[0]
+        #Video streaming route. Put this in the src attribute of an img tag
+        return Response(rr1716.Vision.gen_preview_picker(the_camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+    @app.route("/pick")
+    def pick():
+        app.Cameras[0].get_frame()
+        col = rr1716.Vision.averageColor(app.Cameras[0].get_color(), __COLOR_PICK_RANGE__)
+        print("color: ", col)
+        #write to file
+        file = open("picked_color", "w")
+        file.write(str(int(col[0])) + " " + str(int(col[1])) + " " + str(int(col[2])) + " \n")
+        file.close()
+        return redirect("/preview")
+
+    @app.route("/pick_cone")
+    def pick_cone():
+        app.Cameras[0].get_frame()
+        col = rr1716.Vision.averageColor(app.Cameras[0].get_frame(), __COLOR_PICK_RANGE__)
+        logging.info("cone color: " + str(col))
+        #write to file
+        file = open("cone_picked_color", "w")
+        file.write(str(int(col[0])) + " " + str(int(col[1])) + " " + str(int(col[2])) + " \n")
+        file.close()
+        return redirect("/preview")
+
+    @app.route("/pick_cube")
+    def pick_cube():
+        app.Cameras[0].get_frame()
+        col = rr1716.Vision.averageColor(app.Cameras[0].get_frame(), __COLOR_PICK_RANGE__)
+        logging.info("cube color: " + str(col))
+        #write to file
+        file = open("cube_picked_color", "w")
+        file.write(str(int(col[0])) + " " + str(int(col[1])) + " " + str(int(col[2])) + " \n")
+        file.close()
+        return redirect("/preview")
 
 
 
