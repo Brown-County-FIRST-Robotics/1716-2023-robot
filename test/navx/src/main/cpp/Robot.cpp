d58b410d2c4db81e48da0389969815dc8b02cacb
Colin <cfinn68916@gmail.com>
2023-07-19 16:57:01 -0500

    Add basic navx template
    Signed-off-by: Colin <cfinn68916@gmail.com>

---
+++ test/navx/src/main/cpp/Robot.cpp
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "Robot.h"
+
+
+void Robot::RobotInit() {}
+
+void Robot::RobotPeriodic() {
+}
+
+void Robot::DisabledInit() {}
+
+void Robot::DisabledPeriodic() {}
+
+void Robot::DisabledExit() {}
+
+void Robot::AutonomousInit() {
+}
+
+void Robot::AutonomousPeriodic() {}
+
+void Robot::AutonomousExit() {}
+
+void Robot::TeleopInit() {
+}
+
+void Robot::TeleopPeriodic() {}
+
+void Robot::TeleopExit() {}
+
+
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
