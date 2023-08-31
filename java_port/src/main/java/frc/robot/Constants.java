72bd4ebb2dc8061fcd5792da5b30e81292dc9605
Colin <cfinn68916@gmail.com>
2023-08-31 17:15:38 -0500

    Add pose estimator
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- java_port/src/main/java/frc/robot/Constants.java
+++ java_port/src/main/java/frc/robot/Constants.java
@@ -1,5 +1,10 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
+
 public class Constants {
   public static final class IO {
     // TODO: add real values
@@ -7,5 +12,16 @@ public class Constants {
     public static final int FR_ID = 2;
     public static final int BL_ID = 3;
     public static final int BR_ID = 4;
+  }
+
+  public static final class DRIVETRAIN {
+    public static final MecanumDriveKinematics KINEMATICS =
+        new MecanumDriveKinematics(
+            new Translation2d(0, 0),
+            new Translation2d(0, 0),
+            new Translation2d(0, 0),
+            new Translation2d(0, 0));
+    public static final double EFFECTIVE_WHEEL_DIAMETER = 1;
+    public static final Pose2d INIT_POSE = new Pose2d(1, 1, new Rotation2d(1));
   }
 }
--- java_port/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ java_port/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -3,6 +3,10 @@ package frc.robot.subsystems;
 import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.math.estimator.MecanumDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.MecanumDriveWheelPositions;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -13,9 +17,37 @@ public class Drivetrain extends SubsystemBase {
   CANSparkMax bl = new CANSparkMax(Constants.IO.BL_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
   CANSparkMax br = new CANSparkMax(Constants.IO.BR_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
 
+  RelativeEncoder flEncoder = fl.getEncoder();
+  RelativeEncoder frEncoder = fr.getEncoder();
+  RelativeEncoder blEncoder = bl.getEncoder();
+  RelativeEncoder brEncoder = br.getEncoder();
+
   AHRS navx = new AHRS(SPI.Port.kMXP);
+
+  MecanumDrivePoseEstimator poseEstimator =
+      new MecanumDrivePoseEstimator(
+          Constants.DRIVETRAIN.KINEMATICS,
+          navx.getRotation2d(),
+          getEncoderPositions(),
+          Constants.DRIVETRAIN.INIT_POSE);
 
   public Drivetrain() {
     navx.reset();
+  }
+
+  public void Periodic() {
+    poseEstimator.update(navx.getRotation2d(), getEncoderPositions());
+  }
+
+  public Pose2d getPose() {
+    return poseEstimator.getEstimatedPosition();
+  }
+
+  public MecanumDriveWheelPositions getEncoderPositions() {
+    return new MecanumDriveWheelPositions(
+        flEncoder.getPosition() / Constants.DRIVETRAIN.EFFECTIVE_WHEEL_DIAMETER,
+        frEncoder.getPosition() / Constants.DRIVETRAIN.EFFECTIVE_WHEEL_DIAMETER,
+        blEncoder.getPosition() / Constants.DRIVETRAIN.EFFECTIVE_WHEEL_DIAMETER,
+        brEncoder.getPosition() / Constants.DRIVETRAIN.EFFECTIVE_WHEEL_DIAMETER);
   }
 }
