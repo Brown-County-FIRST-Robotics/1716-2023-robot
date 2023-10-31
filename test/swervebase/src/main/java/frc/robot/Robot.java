254a6775a0055769cfc09bc2c0f92a584e8a7fe2
Colin <cfinn68916@gmail.com>
2023-10-31 16:41:00 -0500

    Adapt code for one module and find offset for FL module
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- test/swervebase/src/main/java/frc/robot/Robot.java
+++ test/swervebase/src/main/java/frc/robot/Robot.java
@@ -1,8 +1,15 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.SwerveModule;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -11,9 +18,12 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
-  private Command autonomousCommand;
 
-  private final RobotContainer robotContainer = new RobotContainer();
+  SwerveModule mod;
+  GenericEntry off;
+    GenericEntry vel;
+
+      GenericEntry inpPos;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -21,8 +31,21 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    double THRUST_DISTANCE_PER_TICK = .0254 * 4.0 * Math.PI / (2048.0 * 6.75);
+    double THRUST_KV = 1023.0 * 600.0 / (6380.0 * 2048.0);
+    double STEER_KV = 1.0/300.0;
+    mod=new SwerveModule(new SwerveModuleConstants(
+            STEER_KV, 0, 0, 0, 0.067, THRUST_KV, 0, 0, 0, THRUST_DISTANCE_PER_TICK, 31, 1));
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
+
+
+    off=Shuffleboard.getTab("TAASAS").add("Offset", mod.constants.steerOffsetRotations).getEntry();
+    vel=Shuffleboard.getTab("TAASAS").add("vel", 0.0).getEntry();
+
+    inpPos=Shuffleboard.getTab("TAASAS").add("inppos", 0.0).getEntry();
+
+
   }
 
   /**
@@ -38,7 +61,10 @@ public class Robot extends TimedRobot {
     // commands, running already-scheduled commands, removing finished or interrupted commands,
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
+
+    mod.constants.steerOffsetRotations=off.getDouble(mod.constants.steerOffsetRotations);
+    //SmartDashboard.putNumber("Pos",mod.constants.steerOffsetRotations+mod.getModulePosition().angle.getRotations());
+
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
@@ -51,12 +77,7 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    autonomousCommand = robotContainer.getAutonomousCommand();
 
-    // schedule the autonomous command (example)
-    if (autonomousCommand != null) {
-      autonomousCommand.schedule();
-    }
   }
 
   /** This function is called periodically during autonomous. */
@@ -65,23 +86,24 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
+
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (autonomousCommand != null) {
-      autonomousCommand.cancel();
-    }
   }
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+        double v=vel.getFloat((float) -0.87);
+    double r=inpPos.getFloat(0);
+    mod.setModuleState(new SwerveModuleState(v,Rotation2d.fromRotations(r)));
+
+  }
 
   @Override
   public void testInit() {
     // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
   }
 
   /** This function is called periodically during test mode. */
--- test/swervebase/src/main/java/frc/robot/subsystems/SwerveModule.java
+++ test/swervebase/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -11,7 +11,7 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import frc.robot.SwerveModuleConstants;
 
 public class SwerveModule {
-  SwerveModuleConstants constants;
+  public SwerveModuleConstants constants;
   CANSparkMax steer;
   WPI_TalonFX thrust;
   SparkMaxAnalogSensor analogSensor;
@@ -21,7 +21,6 @@ public class SwerveModule {
     constants = moduleConstants;
     steer = new CANSparkMax(constants.steerID, CANSparkMaxLowLevel.MotorType.kBrushless);
     thrust = new WPI_TalonFX(constants.thrustID);
-    steer.restoreFactoryDefaults();
     steerPID = steer.getPIDController();
     analogSensor = steer.getAnalog(SparkMaxAnalogSensor.Mode.kAbsolute);
     analogSensor.setPositionConversionFactor(1 / 3.3);
@@ -42,22 +41,22 @@ public class SwerveModule {
     thrust.configFactoryDefault();
     thrust.setNeutralMode(NeutralMode.Brake);
     thrust.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 20);
-    thrust.configNominalOutputForward(0, 200);
-    thrust.configNominalOutputReverse(0, 200);
-    thrust.configPeakOutputForward(1, 200);
-    thrust.configPeakOutputReverse(-1, 200);
-    thrust.config_kP(0, constants.thrustP, 200);
-    thrust.config_kI(0, constants.thrustI, 200);
-    thrust.config_kD(0, constants.thrustD, 200);
-    thrust.config_kF(0, constants.thrustKV, 200);
+    thrust.configNominalOutputForward(0, 20);
+    thrust.configNominalOutputReverse(0, 20);
+    thrust.configPeakOutputForward(1, 20);
+    thrust.configPeakOutputReverse(-1, 20);
+    thrust.config_kP(0, constants.thrustP, 20);
+    thrust.config_kI(0, constants.thrustI, 20);
+    thrust.config_kD(0, constants.thrustD, 20);
+    thrust.config_kF(0, constants.thrustKV, 20);
   }
 
   public void setModuleState(SwerveModuleState cmd_state) {
     SwerveModuleState state = SwerveModuleState.optimize(cmd_state, getModulePosition().angle);
     state.speedMetersPerSecond *= getModulePosition().angle.minus(state.angle).getCos();
     steerPID.setReference(
-        state.angle.getRotations() - constants.steerOffsetRotations,
-        CANSparkMax.ControlType.kPosition);
+        state.angle.getRotations() + constants.steerOffsetRotations,
+        CANSparkMax.ControlType.kSmartMotion);
     thrust.set(
         TalonFXControlMode.Velocity,
         0.1 * state.speedMetersPerSecond / constants.thrustDistancePerTick);
@@ -66,6 +65,6 @@ public class SwerveModule {
   public SwerveModulePosition getModulePosition() {
     return new SwerveModulePosition(
         thrust.getSelectedSensorPosition() * constants.thrustDistancePerTick,
-        Rotation2d.fromRotations(analogSensor.getPosition() + constants.steerOffsetRotations));
+        Rotation2d.fromRotations(analogSensor.getPosition() - constants.steerOffsetRotations));
   }
 }
