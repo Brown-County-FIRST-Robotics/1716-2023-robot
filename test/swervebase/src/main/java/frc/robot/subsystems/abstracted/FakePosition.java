dc98f523e3b3aff1d960a186b5888a3295aa8a44
Colin <cfinn68916@gmail.com>
2023-11-04 11:21:12 -0500

    Add logging to fake motors
    Signed-off-by: Colin <cfinn68916@gmail.com>

--- test/swervebase/src/main/java/frc/robot/subsystems/abstracted/FakePosition.java
+++ test/swervebase/src/main/java/frc/robot/subsystems/abstracted/FakePosition.java
@@ -1,13 +1,19 @@
 package frc.robot.subsystems.abstracted;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class FakePosition implements PositionMotor {
   double internal_pos;
+  private String name;
 
-  public FakePosition() {}
+  public FakePosition(String name_) {
+    name = name_;
+  }
 
   @Override
   public void setPos(double pos) {
     internal_pos = pos;
+    SmartDashboard.putNumber(name + "_pos", pos);
   }
 
   @Override
--- test/swervebase/src/main/java/frc/robot/subsystems/abstracted/FakeVelocity.java
+++ test/swervebase/src/main/java/frc/robot/subsystems/abstracted/FakeVelocity.java
@@ -1,18 +1,23 @@
 package frc.robot.subsystems.abstracted;
 
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class FakeVelocity implements VelocityMotor {
+  private final String name;
   Timer tm = new Timer();
   double pos = 0;
   double lastVel = 0;
 
-  public FakeVelocity() {}
+  public FakeVelocity(String name_) {
+    name = name_;
+  }
 
   @Override
   public void setVelocity(double vel) {
     pos += tm.get() * lastVel;
     lastVel = vel * 10;
+    SmartDashboard.putNumber(name + "_vel", lastVel);
     tm.reset();
     tm.start();
   }
