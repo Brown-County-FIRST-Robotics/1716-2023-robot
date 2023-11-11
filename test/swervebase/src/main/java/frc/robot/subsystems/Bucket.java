ed7043ae61438c848bd38d93d3a7b67fe7724fae
Colin <cfinn68916@gmail.com>
2023-11-11 11:53:43 -0600

    [Glade] bucket changes from testing

--- test/swervebase/src/main/java/frc/robot/subsystems/Bucket.java
+++ test/swervebase/src/main/java/frc/robot/subsystems/Bucket.java
@@ -3,14 +3,16 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Bucket extends SubsystemBase {
   // const, move to constant file eventually
-  private double P = 0.1;
-  private double DOWN_POS = 0.0;
+  private double DOWN_P = 0; // 1;
+  private double UP_P = 0;
+  private double DOWN_POS = -7.5;
 
-  private final WPI_TalonSRX motor = new WPI_TalonSRX(0);
+  private final WPI_TalonSRX motor = new WPI_TalonSRX(12);
   private final Encoder encoder = new Encoder(0, 1);
 
   boolean extended = false;
@@ -29,10 +31,12 @@ public class Bucket extends SubsystemBase {
 
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("Bucket Encoder", encoder.getDistance());
+
     if (extended == false && encoder.getDistance() != 0) {
-      motor.set(P * Math.abs(encoder.getDistance()));
+      motor.set(UP_P * Math.abs(encoder.getDistance()));
     } else if (extended == true && encoder.getDistance() != DOWN_POS) {
-      motor.set(P * Math.abs(encoder.getDistance() - DOWN_POS));
+      motor.set(-DOWN_P * Math.abs(encoder.getDistance() - DOWN_POS));
     } else {
       motor.set(0);
     }
